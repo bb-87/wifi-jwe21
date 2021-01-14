88cb689b0f1d99efbf6e58321eac01547e9f046b
bb-87 <berger.bernhard@gmail.com>
2021-01-14 23:31:31 +0100

    initial commit: jQuery dynamic gallery, lightbox

---
+++ js-uebungen/gallery/js/gallery.js
@@ -0,0 +1,55 @@
+// Liste der Bildnamen in array
+let images = [
+    '001',
+    '002',
+    '003',
+    '004',
+    '005',
+    '006',
+    '007',
+    '008',
+    '009',
+    '010'
+];
+
+let gallery = $('#gallery');
+
+// html-code für das vergrößerte Bild
+let lightboxContainer  = $('<div id="lightbox" class="hide"><span class="close">X</span><div class="lightbox-inner"></div></div>');
+
+$('body > .wrapper').append(lightboxContainer);
+
+// Ausgabe der Thumbs
+// https://api.jquery.com/each/
+$(images).each(
+    function(index, element) {
+        // img tag für thumbnails
+        let htmlImageTag = `<img class="thumb" src="img/thumbs/${element}.jpg" alt="">`;
+
+        // img link zum original bild
+        htmlImageTag = `<a href="img/original/${element}.jpg"> ${htmlImageTag} </a>`;
+
+        // gallery.html(htmlImageTag); // ersetzt
+        gallery.append(htmlImageTag); // fügt hinzu
+        // console.log(htmlImageTag);
+    }
+);
+
+// event handler für klick auf das Thumbnail
+$('#gallery a').click(
+    function(event) {
+        event.preventDefault(); // Default Browserverhalten bei Klick unterdrücken
+        let urlToOriginalImg = $(this).attr('href');
+        lightboxContainer.find('.lightbox-inner').html(`<img src="${urlToOriginalImg}" alt="">`);
+        lightboxContainer.removeClass('hide');
+
+    }
+);
+
+// bei Klick auf span X hide-Klasse wieder entfernen
+$('#lightbox span').click(
+    function(event) {
+        event.preventDefault();
+        lightboxContainer.addClass('hide');
+    }
+);
