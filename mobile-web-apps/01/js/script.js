0e1049848f0e4b155f82387086c6a35132df2a41
bb-87 <berger.bernhard@gmail.com>
2021-02-22 17:08:49 +0100

    small changes in v1, added v2 and v3

--- mobile-web-apps/01/js/script.js
+++ mobile-web-apps/01/js/script.js
@@ -1,20 +1,21 @@
+/* 1st version
 function calculate() {
     // Eingabe in 2 Variablen einlesen und Operator einlesen
     const num1 = parseFloat(document.getElementById('num1').value); // "2" und nicht 2 -> parseInt (parseFloat für Dezimalzahlen)
     const num2 = parseFloat(document.getElementById('num2').value);
     const operator = document.getElementById('operator').value;
 
-    // Berechnung und speichern in einer dritte Variable
+    // Berechnung und speichern in einer dritten Variable
 
     var result;
 
-    if (operator == '+') {
+    if (operator === '+') {
         result = num1 + num2;
-    } else if (operator == '-') {
+    } else if (operator === '-') {
         result = num1 - num2;
-    } else if (operator == '*') {
+    } else if (operator === '*') {
         result = num1 * num2;
-    } else if (operator == '/') {
+    } else if (operator === '/') {
         result = num1 / num2;
     }
 
@@ -25,3 +26,138 @@ function calculate() {
 const button = document.getElementById('button-calc');
 
 button.addEventListener('click', calculate);
+*/
+
+
+/* 2nd version
+function parseInput(number_s) {
+    if (number_s.trim().toUpperCase() === 'PI') {
+        return Math.PI;
+    }
+    else if (number_s.trim().toUpperCase() === 'E') {
+        return Math.E;
+    }
+    else {
+        return parseFloat(number_s);
+    }
+}
+
+function calculate() {
+    const num1 = parseInput(document.querySelector('#num1').value);
+    const num2 = parseInput(document.querySelector('#num2').value);
+    const operator = document.querySelector('#operator').value;
+
+    // Berechnung und speichern in einer dritten Variable
+    let result;
+
+    if (operator === '+') {
+        result = num1 + num2;
+    }
+    else if (operator === '-') {
+        result = num1 - num2;
+    }
+    else if (operator === '*') {
+        result = num1 * num2;
+    }
+    else if (operator === '/') {
+        result = num1 / num2;
+    }
+
+    // Die Ausgabe
+    document.querySelector('#result').value = result;
+};
+
+const button = document.querySelector('#button-calc');
+
+button.addEventListener('click', calculate);
+*/
+
+
+// 3rd version
+function parseInput(number_s, defaultValue) {
+    if (number_s.trim().toUpperCase() === 'PI') {
+        return Math.PI;
+    }
+    else if (number_s.trim().toUpperCase() === 'E') {
+        return Math.E;
+    }
+    else {
+        const number = parseFloat(number_s);
+
+        if (isNaN(number)) {
+            return defaultValue;
+        }
+        else {
+            return number;
+        }
+    }
+}
+
+function calculate() {
+    const num1 = parseInput(document.querySelector('#num1').value);
+    const num2 = parseInput(document.querySelector('#num2').value);
+    const operator = document.querySelector('#operator').value;
+
+    // Berechnung und speichern in einer weiteren Variable
+    let result;
+
+    if (operator === '+') {
+        result = num1 + num2;
+    }
+    else if (operator === '-') {
+        result = num1 - num2;
+    }
+    else if (operator === '*') {
+        result = num1 * num2;
+    }
+    else if (operator === '/') {
+        result = num1 / num2;
+    }
+
+    // Die Ausgabe
+    document.querySelector('#result').value = result;
+}
+
+function getNumbers(defaultValue) {
+    const numberElems = document.querySelectorAll('.number');
+
+    let numbers = [];
+    for (let i = 0; i < numberElems.length; i++) {
+        numbers.push(parseInput(numberElems[i].value, defaultValue));
+    }
+    console.log(numbers);
+    return numbers;
+}
+
+function sum() {
+    const numbers = getNumbers(0);
+
+    // let result = 0;
+    // for (let number of numbers) {
+    //     result += number;
+    // }
+
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        result += numbers[i];
+    }
+
+    document.querySelector('#result').value = result;
+}
+
+function product() {
+    const numbers = getNumbers(1);
+
+    let result = 1;
+    for (let number of numbers) {
+        result *= number;
+    }
+
+    document.querySelector('#result').value = result;
+}
+
+const button = document.querySelector('#button-calc');
+button.addEventListener('click', calculate);
+
+document.querySelector('#button-sum').addEventListener('click', sum);
+document.querySelector('#button-product').addEventListener('click', product);
