d794dc7426065386e048ff103be92274c32b0c7b
bb-87 <berger.bernhard@gmail.com>
2021-02-26 20:40:48 +0100

    added project files

---
+++ php1/projekt/header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <ul>
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="index.php">Leistungen</a></li>
+            <li><a href="index.php">Öffnungszeiten</a></li>
+            <li><a href="index.php">Kontakt</a></li>
+        </ul>
+    </nav>
+    <figure>
+        <img src="images/stage.jpg" alt="" />
+    </figure>
+</header>
---
+++ php1/projekt/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+
+<head>
+    <title>WIFI Demo</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="css/screen.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <?PHP
+
+    // HTML blockweise wieder zusammensetzen
+
+    include "header.php";
+
+    include "content/home.php";
+
+    include "footer.php";
+
+    ?>
+
+</body>
+
+</html>
