f00bc579ce0f3237a0ba4a6a90330b884b3ef686
bb-87 <berger.bernhard@gmail.com>
2021-03-03 14:08:54 +0100

    rewrote code for Set and Map objects

--- mobile-web-apps/03/js/random-list.js
+++ mobile-web-apps/03/js/random-list.js
@@ -16,10 +16,32 @@ class RandomList {
      */
     constructor(min, max, length) {
         this._list = [];
+        // // Option 3: Zusätzliches Erzeugen eines Sets
+        // this._set = new Set(); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/Set
+
+        // Option 4: 
+        this._map = new Map();
 
         for (let i = 0; i < length; i++) {
-            this._list.push(randomInt(min, max));
+            const randomNumber = randomInt(min, max);
+
+            this._list.push(randomNumber);
+            // Option 3: Set updaten
+            // this._set.add(randomNumber);
+
+            // Option 4: Map updaten
+            if (this._map.has(randomNumber)) {
+                const cnt = this._map.get(randomNumber);
+                this._map.set(randomNumber, cnt + 1);
+            } else {
+                this._map.set(randomNumber, 1);
+            }
         }
+
+        // // Option 2: Sortieren der Liste
+        // this._list.sort(function compare(a, b) {
+        //     return a - b; // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#description
+        // });
     }
 
     /**
@@ -27,12 +49,24 @@ class RandomList {
      * @param {Number} val 
      */
     isInList(val) {
-        for (let i = 0; i < this._list.length; i++) {
-            if (this._list[i] === val) {
-                return true;
-            }
-        }
-        return false;
+        // // Option 1: Suchen in der Liste nach einem Wert
+        // for (let i = 0; i < this._list.length; i++) {
+        //     if (this._list[i] === val) {
+        //         return true;
+        //     }
+        //     // Option 2: Abbrechen, wenn der Wert in einer geordneten Liste nicht mehr gefunden werden kann 
+        //     else if (this._list[i] > val) {
+        //         return false;
+        //     }
+        // }
+        // return false;
+
+        // // Option 3: Nutzen von Set
+        // // set.has() returns true or false
+        // return this._set.has(val); 
+
+        // Option 4: Nutzen von Map. Wenn val kein Schlüssel in der Map ist, kommt value nicht vor.
+        return this._map.has(val);
     }
 
     /**
@@ -41,13 +75,19 @@ class RandomList {
      * @returns {Number} The number of occurences
      */
     count(val) {
-        let cnt = 0;
+        // // Option 1: Durchsuchen der Liste 
+        // let cnt = 0;
 
-        for (let i = 0; i < this._list.length; i++) {
-            if (this._list[i] === val) {
-                cnt++;
-            }
-        }
-        return cnt;
+        // for (let i = 0; i < this._list.length; i++) {
+        //     if (this._list[i] === val) {
+        //         cnt++;
+        //     } else if (this._list[i] > val) {
+        //         return cnt;
+        //     }
+        // }
+        // return cnt;
+
+        // Option 2: Nutzen von Map
+        return this._map.get(val);
     }
 }
